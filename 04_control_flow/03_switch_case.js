c88fc689cd2ac5befa29e653745f9f80ae260feb
ItsMe1104 <78407494+ItsMe1104@users.noreply.github.com>
2024-01-19 10:34:01 +0000

    Added switch_case file

---
+++ 04_control_flow/03_switch_case.js
@@ -0,0 +1,24 @@
+// switch case :- Just like multiple if-else statements
+// It is mainly used for checking equality of a value with a series of other values. 
+// Whichever value from that series of value proves to be equal, we execute that block of code
+
+// 1) Syntax :-
+/*
+switch (key) {
+    case value:
+        ...
+        break;
+
+    default:
+        break;
+}
+*/
+// key is the value we want to check
+// default: :- is the default case which runs its block if no other cases from above matches the required value and runs their block
+
+// Since we do not define blocks for every cases with braces {}, hence we need to add a 'break' statement for every case block marking its end. 
+
+//If we dont put break statement then it will execute all the cases below it till the default case or if it encounters a break statement in between.
+
+
+
