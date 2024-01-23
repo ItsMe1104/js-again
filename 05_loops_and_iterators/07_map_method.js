6c9983b4c576d118fc6f416286d9fb33d821e513
ItsMe1104 <78407494+ItsMe1104@users.noreply.github.com>
2024-01-23 10:25:48 +0000

    Updated map method file and renamed it, Added Reduce method file.

--- 05_loops_and_iterators/07_map_method.js
+++
@@ -1,2 +0,0 @@
-//Map method in arrays
-//Best replacement for ForEach loop
---
+++ 05_loops_and_iterators/08_reduce_method.js
@@ -0,0 +1,14 @@
+// Reduce Method :- executes an user supplied reducer
+// At last returns a single value
+/*
+Syntax :-
+
+    arr_name.reduce(callback_func(x,y){
+        return ...
+    } , z)
+
+    x = accumulator
+    y = current element value from array
+    z = initial value
+    
+*/
