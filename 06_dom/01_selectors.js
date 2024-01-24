d88202152729b40c3635309eda0d460150b97f63
ItsMe1104 <78407494+ItsMe1104@users.noreply.github.com>
2024-01-24 08:58:58 +0000

    Added files for learning Dom manipulation.

---
+++ 06_dom/01_selectors.js
@@ -0,0 +1,7 @@
+//Selectors :- help us to target a specific HTML element based on its attributes so that we can perform operations on it.
+
+
+//1) GetElementById("id_name")  :- Targets elements by their id
+// returns a single element as ids are always unique.
+
+document.getElementById("title");
