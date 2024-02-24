d905d39c8b3a091f82438a7f05977ab0113f85f1
ItsMe1104 <78407494+ItsMe1104@users.noreply.github.com>
2024-02-24 08:46:10 +0000

    Started asynchronous JS and added files for that.

---
+++ 010_asynchronous_part/one.js
@@ -0,0 +1,5 @@
+//Asynchronous code in JS
+
+//setTimeout() :- It is a method which also calls an API
+//The window object in the console has options like setInterval, setTimeout, clearInterval, clearTimeout which are mapped with their respective functions 
+//Hence these functions can only be found in browser and other 
