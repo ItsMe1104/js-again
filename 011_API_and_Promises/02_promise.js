57b2bdebe2293e7f91b3a61b687071d233e7aa3a
ItsMe1104 <78407494+ItsMe1104@users.noreply.github.com>
2024-02-27 05:16:31 +0000

    Added new files for Apis and promises.

---
+++ 011_API_and_Promises/02_promise.js
@@ -0,0 +1,20 @@
+//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+//It is an object, hence to create it use "new" keyword
+
+
+// A promise is in one of these states 
+
+
+//pending :- initial state, neither fulfilled nor rejected.
+//fulfilled :- meaning that the operation was completed successfully
+//rejected :- meaning that the operation has failed.
+
+
+
+//1) Creating promises :-
+const promiseOne = new Promise(function(resolve, reject) {
+    //Do an async task
+    //DB calls, cryptography, network'
+
+    
+});
