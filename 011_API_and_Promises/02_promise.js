0c8c7f5d549be53cb8daff8c00716a833672d33b
ItsMe1104 <78407494+ItsMe1104@users.noreply.github.com>
2024-03-02 21:12:20 +0000

    Updated apiand request file

--- 011_API_and_Promises/02_promise.js
+++ 011_API_and_Promises/02_promise.js
@@ -1,20 +1,39 @@
 //The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+//operations like network stream, file access from system.
+//promise future mei jaake complete hota hai
+
 //It is an object, hence to create it use "new" keyword
 
 
 // A promise is in one of these states 
 
 
-//pending :- initial state, neither fulfilled nor rejected.
-//fulfilled :- meaning that the operation was completed successfully
-//rejected :- meaning that the operation has failed.
+// pending :- initial state, neither fulfilled nor rejected.
+// fulfilled :- meaning that the operation was completed successfully
+// rejected :- meaning that the operation has failed.
 
 
 
 //1) Creating promises :-
+// Promise(a(b,c){...})
+// a --> callback function
+// b --> resolve :- promise is fulfilled
+// c --> reject :- promise is not fulfilled
+
 const promiseOne = new Promise(function(resolve, reject) {
     //Do an async task
-    //DB calls, cryptography, network'
+    //DB calls, cryptography, network 
 
-    
+    setTimeout(() => {
+        console.log("Async task is completed");
+    }, 1000 );
 });
+
+
+
+//************************************************************************************************************************* */
+// 2) Consuming the promise
+// .then() is used to consume promises
+// . then() has clear relation with resove
+
+// .then(a,b)
